99a288f2d0a628273dc57b49b1ee3d8eb27fd7d0
Muhammad Zulfi Rusdani <donimuzur@gmail.com>
2022-10-19 16:09:32 +0700

    fix villager stats

--- FullyEquipment/Main.cs
+++ FullyEquipment/Main.cs
@@ -7,6 +7,7 @@ using FullyEquipment.Helper;
 using Il2CppSystem.Collections.Generic;
 using UnityEngine.SceneManagement;
 using System;
+using System.Linq;
 
 namespace FullyEquipment
 {
@@ -16,16 +17,39 @@ namespace FullyEquipment
         public const string Description = "This mod will force command villager to seek weapon and armor";
         public const string Author = "donimuzur"; // Author of the Mod.  (MUST BE SET)
         public const string Company = null; // Company that made the Mod.  (Set as null if none)
-        public const string Version = "1.0.0"; // Version of the Mod.  (MUST BE SET)
+        public const string Version = "1.1.0"; // Version of the Mod.  (MUST BE SET)
         public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)    
     }
     public class FullyEquipment : MelonMod
     {
-        public bool isFinish; 
+        public bool isFinish;
+        public bool init;
+        public int lastCount = 0;
         public GameManager gameManager = null;
         public bool isVillagerModExist = false;
         public InputManager inputManager = null;
         public GameObject selectedBuilding = null;
+        public static event Action<VillagerOccupation.Occupation> onOccupationChanged;
+        public static event Action<string> onVillagerJoinedAdded;
+        public static ItemStats powerfullStats = new ItemStats()
+        {
+            armorModifier = 100,
+            meleeDamageIncrease = 100,
+            maxLifeModifier = 0,
+            meleeBlockChance = 100,
+            rangedBlockChance = 100,
+            rangedDamageIncrease = 100
+        };
+        public static ItemStats hunterStats = new ItemStats()
+        {
+            armorModifier = 0,
+            meleeDamageIncrease = 3,
+            maxLifeModifier = 25,
+            meleeBlockChance = 0,
+            rangedBlockChance = 0,
+            rangedDamageIncrease = 3
+        };
+        public static ItemStats itemStats;
         public override void OnApplicationStart()
         {
             MelonLogger.Msg("BasicWeaponEquipment Started");
@@ -37,22 +61,45 @@ namespace FullyEquipment
                     isVillagerModExist = true;
                 }
             }
+            itemStats = hunterStats;
         }
         public override void OnSceneWasLoaded(int buildIndex, string sceneName)
         {
             isFinish = false;
-            if(sceneName == "Map")
+            init = false;
+            
+            var gameManagerObj = GameObject.FindObjectOfType<GameManager>();
+            if(gameManagerObj != null)
             {
-                removedUnusedItem();
-                EquipBasicWeapon();
+                lastCount= gameManagerObj.villageStats.villagersJoinedTotal;
             }
         }
-        void test(string str)
+        public bool checkVillagerCountChanged()
         {
-            MelonLogger.Msg(str);
+            var villageStats = GameObject.FindObjectOfType<VillageStats>();
+            if (villageStats != null)
+            {
+                var newCount= villageStats.villagersJoinedTotal;
+                if(lastCount != newCount)
+                {
+                    lastCount = newCount;
+                    return true;
+                }
+                return false;
+            }
+            return false;
         }
         public override void OnUpdate()
         {
+            if (checkVillagerCountChanged())
+            {
+                MelonLogger.Msg("check villager Count");
+                removedUnusedItem();
+                EquipBasicWeapon();
+            }
+
+            if (isFinish) return;
+
             var gameManagerObj = GameObject.Find("GameManager");
             if (gameManagerObj != null)
             {
@@ -61,9 +108,6 @@ namespace FullyEquipment
 
                 if (selectedBuilding != null && selectedBuilding.tag == "TownCenter" && !isFinish)
                 {
-                    var villagerStats = gameManagerObj.GetComponent<VillageStats>();
-                    villagerStats.BindOnVillagerJoinedAdded(new Action<string>(test));
-                    GameObject.Destroy(GameObject.Find("ForceEquipWeaponButton"));
                     var getUIpausWindowList = Resources.FindObjectsOfTypeAll(Il2CppType.From(typeof(UIPauseWindow)));
 
                     if (getUIpausWindowList != null && getUIpausWindowList.Count > 0)
@@ -122,70 +166,176 @@ namespace FullyEquipment
                                 {
                                     uiButtonRectTransofrm.localPosition = new Vector3(400, -30, 0);
                                 }
+
                                 isFinish = true;
                             }
                         }
                     }
-                }
-                else if (selectedBuilding == null)
-                {
-                    isFinish = false;
                 }
             }
         }
         void removedUnusedItem()
         {
             var villagerList = GameObject.FindObjectsOfType<Villager>();
+            
+            foreach(var occupation in Enum.GetValues(typeof(VillagerOccupation.Occupation)).Cast<VillagerOccupation.Occupation>())
+            {
+                if (VillagerHealth.aggressiveAnimalFighters.Contains(occupation)) continue;
+                VillagerHealth.aggressiveAnimalFighters.Add(occupation);
+                VillagerOccupation.acquiredBearAsTargetResponders.Add(occupation);
+            }
             foreach (var villager in villagerList)
             {
-                villager.add_onOccupationChanged(new Action<VillagerOccupation.Occupation>(ocOccupationChanged));
+                onOccupationChanged = delegate (VillagerOccupation.Occupation occupation)
+                {
+
+                    if (villager.GetOccupation() == VillagerOccupation.Occupation.Soldier ||
+                    villager.GetOccupation() == VillagerOccupation.Occupation.Hunter ||
+                    villager.GetOccupation() == VillagerOccupation.Occupation.Guard)
+                    {
+                        //villager.equipmentManager.baseItemStats = itemStats;
+                        //villager.equipmentManager.items = itemStats; 
+                        return;
+                    }
+                    
+                    var getUnusedItemsList = villager.occupation.unusedItemsToFree;
+                    getUnusedItemsList.Contains(new ItemWeapon());
+
+                    villager.combatComp.canAttackBack = true;
+                    villager.combatComp.defaultIsMeleeAttack = true;
+                    villager.combatComp.trainingLevel = 30;
+                    villager.combatComp.searchRange = 90;
+                    villager.combatComp.applyChaseRetreatingTargetRules = false;
+                    villager.combatComp.teamDef = villager.combatManager.guardTowerTeamDefinition;
+                    villager.combatComp.combatTargetType = CombatTargetType.LargeThreat;
+                    villager.equipmentManager.baseItemStats = itemStats;
+                    villager.equipmentManager.equipmentItemStats = itemStats;
+                    var occupationSelected = villager.occupation.TryCast<VillagerOccupationWantsAndNeeds>();
+                    var villagerRetreatSearchEntry = occupationSelected.villagerRetreatSearchEntry;
+                    if (villagerRetreatSearchEntry != null)
+                    {
+                        villagerRetreatSearchEntry.alwaysRetreatIfHaveResidence = false;
+                        villagerRetreatSearchEntry.sqrMagnitude = 8100;
+                    }
+                    var villagerAttackTargetSearchEntry = occupationSelected.villagerAttackTargetSearchEntry;
+                    if (villagerAttackTargetSearchEntry != null)
+                    {
+                        villagerAttackTargetSearchEntry.sqrMagnitude = 8100;
+                    }
+
+                    //var listToRemove = new List<Item>();
+                    //foreach (var item in getUnusedItemsList)
+                    //{
+                    //    if (item.itemID == ItemID.Weapon || item.itemID == ItemID.Bow || item.itemID == ItemID.Crossbow || item.itemID == ItemID.Arrow)
+                    //    {
+                    //        listToRemove.Add(item);
+                    //    }
+                    //}
+
+                    //foreach (var toRemove in listToRemove)
+                    //{
+                    //    villager.occupation.unusedItemsToFree.Remove(toRemove);
+                    //}
+
+                    //var seekItemList1 = new List<SeekItemEntry>();
+                    //seekItemList1.Add(new SeekItemEntry(ItemID.Weapon, 1, 0));
+                    //var seekItemGroup1 = new SeekItemGroup() { interaction = SeekItemGroup.GroupInteraction.Upgrade, entries = seekItemList1 };
+
+                    //var seekItemList2 = new List<SeekItemEntry>();
+                    //seekItemList2.Add(new SeekItemEntry(ItemID.Bow, 1, 0));
+                    //seekItemList2.Add(new SeekItemEntry(ItemID.Crossbow, 1, 0));
+                    //var seekItemGroup2 = new SeekItemGroup() { interaction = SeekItemGroup.GroupInteraction.Upgrade, entries = seekItemList2 };
+
+                    //var seekItemList3 = new List<SeekItemEntry>();
+                    //seekItemList3.Add(new SeekItemEntry(ItemID.Arrow, 20, 0));
+                    //var seekItemGroup3 = new SeekItemGroup() { interaction = SeekItemGroup.GroupInteraction.RequireAll, entries = seekItemList3 };
+
+                    //var seekItemGroupList = new List<SeekItemGroup>();
+                    //seekItemGroupList.Add(seekItemGroup1);
+                    //seekItemGroupList.Add(seekItemGroup2);
+                    //seekItemGroupList.Add(seekItemGroup3);
+
+                    //villager.itemRequester.SetItemCriteriaToSeek(seekItemGroupList);
+                };
+                villager.add_onOccupationChanged(new Action<VillagerOccupation.Occupation>(onOccupationChanged));
             }
-        }
-        private void ocOccupationChanged(VillagerOccupation.Occupation occupation)
-        {
-            EquipBasicWeapon();
         }
         void EquipBasicWeapon()
         {
+            foreach (var occupation in Enum.GetValues(typeof(VillagerOccupation.Occupation)).Cast<VillagerOccupation.Occupation>())
+            {
+                if (VillagerHealth.aggressiveAnimalFighters.Contains(occupation)) continue;
+                VillagerHealth.aggressiveAnimalFighters.Add(occupation);
+                VillagerOccupation.acquiredBearAsTargetResponders.Add(occupation);
+            }
             var villagerList = GameObject.FindObjectsOfType<Villager>();
             foreach (var villager in villagerList) 
             {
-                var getUnusedItemsList = villager.occupation.unusedItemsToFree;
-                getUnusedItemsList.Contains(new ItemWeapon());
-
-                var listToRemove = new List<Item>();
-                foreach (var item in getUnusedItemsList)
+                if (villager.GetOccupation() == VillagerOccupation.Occupation.Soldier || villager.GetOccupation() == VillagerOccupation.Occupation.Hunter || villager.GetOccupation() == VillagerOccupation.Occupation.Guard) 
                 {
-                    if (item.itemID == ItemID.Weapon || item.itemID == ItemID.Bow || item.itemID == ItemID.Crossbow || item.itemID == ItemID.Arrow)
-                    {
-                        listToRemove.Add(item);
-                    }
+                    //villager.equipmentManager.baseItemStats = itemStats;
+                    //villager.equipmentManager.equipmentItemStats = itemStats;
+                    continue;
+                }
+                
+                villager.combatComp.canAttackBack = true;
+                villager.combatComp.defaultIsMeleeAttack = true;
+                villager.combatComp.trainingLevel = 30;
+                villager.combatComp.searchRange = 90;
+                villager.combatComp.applyChaseRetreatingTargetRules = false;
+                villager.combatComp.teamDef = villager.combatManager.guardTowerTeamDefinition;
+                villager.combatComp.combatTargetType = CombatTargetType.LargeThreat;
+                villager.equipmentManager.baseItemStats = itemStats;
+                villager.equipmentManager.equipmentItemStats = itemStats;
+                var occupationSelected = villager.occupation.TryCast<VillagerOccupationWantsAndNeeds>();
+                var villagerRetreatSearchEntry = occupationSelected.villagerRetreatSearchEntry;
+                if (villagerRetreatSearchEntry != null)
+                {
+                    villagerRetreatSearchEntry.alwaysRetreatIfHaveResidence = false;
+                    villagerRetreatSearchEntry.sqrMagnitude = 8100;
+                }
+                var villagerAttackTargetSearchEntry = occupationSelected.villagerAttackTargetSearchEntry;
+                if (villagerAttackTargetSearchEntry != null)
+                {
+                    villagerAttackTargetSearchEntry.sqrMagnitude = 8100;
                 }
 
-                foreach (var toRemove in listToRemove)
-                {
-                    villager.occupation.unusedItemsToFree.Remove(toRemove);
-                }
+                //var getUnusedItemsList = villager.occupation.unusedItemsToFree;
+                //getUnusedItemsList.Contains(new ItemWeapon());
+                //var listToRemove = new List<Item>();
+                //foreach (var item in getUnusedItemsList)
+                //{
+                //    if (item.itemID == ItemID.Weapon || item.itemID == ItemID.Bow || item.itemID == ItemID.Crossbow || item.itemID == ItemID.Arrow)
+                //    {
+                //        listToRemove.Add(item);
+                //    }
+                //}
+
+                //foreach (var toRemove in listToRemove)
+                //{
+                //    villager.occupation.unusedItemsToFree.Remove(toRemove);
+                //}
 
                 var seekItemList1 = new List<SeekItemEntry>();
+                seekItemList1.Add(new SeekItemEntry(ItemID.SimpleWeapon, 1, 0));
                 seekItemList1.Add(new SeekItemEntry(ItemID.Weapon, 1, 0));
                 var seekItemGroup1 = new SeekItemGroup() { interaction = SeekItemGroup.GroupInteraction.Upgrade, entries = seekItemList1 };
 
-                var seekItemList2 = new List<SeekItemEntry>();
-                seekItemList2.Add(new SeekItemEntry(ItemID.Bow, 1, 0));
-                seekItemList2.Add(new SeekItemEntry(ItemID.Crossbow, 1, 0));
-                var seekItemGroup2 = new SeekItemGroup() { interaction = SeekItemGroup.GroupInteraction.Upgrade, entries = seekItemList2 };
+                //var seekItemList2 = new List<SeekItemEntry>();
+                //seekItemList2.Add(new SeekItemEntry(ItemID.Bow, 1, 0));
+                //seekItemList2.Add(new SeekItemEntry(ItemID.Crossbow, 1, 0));
+                //var seekItemGroup2 = new SeekItemGroup() { interaction = SeekItemGroup.GroupInteraction.Upgrade, entries = seekItemList2 };
 
-                var seekItemList3 = new List<SeekItemEntry>();
-                seekItemList3.Add(new SeekItemEntry(ItemID.Arrow, 20, 0));
-                var seekItemGroup3 = new SeekItemGroup() { interaction = SeekItemGroup.GroupInteraction.RequireAll, entries = seekItemList3 };
+                //var seekItemList3 = new List<SeekItemEntry>();
+                //seekItemList3.Add(new SeekItemEntry(ItemID.Arrow, 20, 0));
+                //var seekItemGroup3 = new SeekItemGroup() { interaction = SeekItemGroup.GroupInteraction.RequireAll, entries = seekItemList3 };
 
-                var seekItemGroupList = new List<SeekItemGroup>();
-                seekItemGroupList.Add(seekItemGroup1);
-                seekItemGroupList.Add(seekItemGroup2);
-                seekItemGroupList.Add(seekItemGroup3);
+                //var seekItemGroupList = new List<SeekItemGroup>();
+                //seekItemGroupList.Add(seekItemGroup1);
+                //seekItemGroupList.Add(seekItemGroup2);
+                //seekItemGroupList.Add(seekItemGroup3);
 
-                villager.itemRequester.SetItemCriteriaToSeek(seekItemGroupList);
+                //villager.itemRequester.SetItemCriteriaToSeek(seekItemGroupList);
             }
         }
     }
